928f3d332b2592dcdef6e27c2fc5ae508ecb84f8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-03 16:52:01 -0300

    chore: base db context

---
+++ Goals.API/Context/DatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Goals.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Goals.API.Context
+{
+    public class DatabaseContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<UserModel> Users { get; protected set; }
+    }
+}

07b6abf2210a28d2c1d69a3673b9baf72f8f4a40
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-03 16:50:30 -0300

    chore: login attempt at user model

--- Goals.API/Models/UserModel.cs
+++ Goals.API/Models/UserModel.cs
@@ -10,6 +10,7 @@ namespace Goals.API.Models
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public int LoginAttempt { get; set; } = 0;
         public string PasswordHash { get; set; } = string.Empty;
         public string PasswordSalt { get; set; } = string.Empty;
     }

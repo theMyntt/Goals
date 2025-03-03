01d3e1a8d0958325087a0820059f3c0a81d1f3f2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-03 17:27:07 -0300

    chore: jwt handler

---
+++ Goals.API/Abstractions/Helpers/IJwtHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Goals.API.Abstractions.Helpers
+{
+    public interface IJwtHelper
+    {
+        /// <summary>
+        /// Wait name and email of user and give you a 7 days Jwt Token
+        /// </summary>
+        /// <param name="name">
+        /// Name of User
+        /// </param>
+        /// <param name="email">
+        /// Email of User
+        /// </param>
+        /// <returns>
+        /// 7 days Jwt Token
+        /// </returns>
+        string GenerateToken(string name, string email);
+    }
+}
---
+++ Goals.API/Helpers/JwtHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Goals.API.Abstractions.Helpers;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Goals.API.Helpers
+{
+    public class JwtHelper : IJwtHelper
+    {
+        private readonly SigningCredentials _creds;
+        private readonly IConfigurationSection _jwt;
+
+        public JwtHelper(IConfiguration configuration)
+        {
+            var jwtSection = configuration.GetSection("Jwt");
+            _jwt = jwtSection;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection["SecretKey"]!));
+            _creds = new(key, SecurityAlgorithms.HmacSha256);
+        }
+
+        public string GenerateToken(string name, string email)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.Email, email),
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _jwt["Issuer"]!,
+                audience: _jwt["Audience"]!,
+                claims: claims,
+                expires: DateTime.Now.AddDays(7),
+                signingCredentials: _creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}

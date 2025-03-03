0b0e021c112b1e5936a8c0afd7d8d77ab2162c6c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-03 16:49:49 -0300

    chore: user basic model

---
+++ Goals.API/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Goals.API.Models
+{
+    [Table("Users")]
+    public class UserModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PasswordSalt { get; set; } = string.Empty;
+    }
+}

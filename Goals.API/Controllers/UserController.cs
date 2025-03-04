dcf6103af623dce4e7f5624c74be3b9078bf98e8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-03 21:14:56 -0300

    chore: login rest route

---
+++ Goals.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Goals.API.Abstractions.Services;
+using Goals.API.DTOs.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Goals.API.Controllers
+{
+    [ApiController]
+    [Route("/api/user/")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("v1/login")]
+        public async Task<IActionResult> Login(LoginInputDTO dto)
+        {
+            var result = await _service.Login(dto);
+
+            return Ok(result);
+        }
+    }
+}

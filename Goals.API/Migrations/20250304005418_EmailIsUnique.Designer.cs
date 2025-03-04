cf11cef174739ef58e3346cfa2627a78582a3394
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-03 21:54:58 -0300

    ci: email is unique migration

--- Goals.API/Migrations/20250304005418_EmailIsUnique.Designer.cs
+++ Goals.API/Migrations/20250304005418_EmailIsUnique.Designer.cs
@@ -4,6 +4,7 @@ using Goals.API.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Goals.API.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20250304005418_EmailIsUnique")]
+    partial class EmailIsUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

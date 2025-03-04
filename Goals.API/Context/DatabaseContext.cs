382d86b6621d8b23ccff9adc5e10f97356c94db3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-03-03 21:53:19 -0300

    chore: email is unique

--- Goals.API/Context/DatabaseContext.cs
+++ Goals.API/Context/DatabaseContext.cs
@@ -6,5 +6,17 @@ namespace Goals.API.Context
     public class DatabaseContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<UserModel> Users { get; protected set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>(builder =>
+            {
+                builder
+                    .HasIndex(u => u.Email)
+                    .IsUnique();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
